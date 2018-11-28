cdb53320ce64364b9215859e27190c63fa2119bc
EunjeOh <oeunjae1004@sasa.hs.kr>
2018-11-28 10:16:17 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import os, random
+
+item_list = {'커피': 5, '밀가루': 5, '희토류': 5, '석유': 5, '소고기': 5, '시멘트': 5, '알루미늄': 5, '강철': 5]
+turns = 5
+             
 def get_input(current_list):
     pass
 '''
@@ -22,17 +27,47 @@ def get_input(current_list):
     #
     #
 
-def get_text():
-    pass
+def get_price_change(item_list): 
 '''
 각 턴에서 사용될 물건 시세 변동여부와 공급/수요 여부 텍스트 파일을 만들기
 '''
+    gloabal turns 
+             
+    flist = os.listdir('price_data')
 
-def print_price_text():
-    pass
-'''
-물건 시세 변동 여부와 공급/수요 여부 텍스트 파일 출력하기
-'''
+    for file in flist:
+        fr = open('price_data\\' + file, 'r')
+        lines = fr.readlines()
+        random.shuffle(lines)
+        fr.close()
+
+    for cnt in range(turns):
+        newname = 'price_change_' + str(cnt) + '.txt'
+        fw = open('price_change\\' + newname, 'w')
+        
+        for file in flist:
+            name = os.path.split(file)[0]
+            fr = open('price_data\\' + file, 'r')
+            lines = fr.readlines()
+            price = name + ':' + lines[cnt]
+            fw.write(price)
+            fr.close()
+             
+        fw.close()
+             
+def print_price_text(turns):
+    '''
+    물건 시세 변동 여부와 공급/수요 여부 텍스트 파일 출력하기
+    '''
+
+    file = 'price_change_' + str(turns) + '.txt'
+    fr = open('price_change\\' + file, 'r')
+    while True:
+        line = fr.readline()
+        if not line: 
+            break
+        print(line)
+    fr.close()
 
 def get_ind_score():
     pass
@@ -65,13 +100,10 @@ print('='*50)
 print('economic game')
 print('='*50)
 
-item_list = {'커피': 5, '밀가루': 5, '희토류': 5, '석유': 5, '소고기': 5, '시멘트': 5, '알루미늄': 5, '강철': 5]
-turns = 5
-
-price_text()
+get_price_change(item_list)
 
 for i in range(turns):
-    print_price()
+    print_price_text(i)
     trading(item_list)
     print_ind_score()
     print_all_score()
