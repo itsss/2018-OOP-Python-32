b9c5e2b923321a55ba8eefc5c1815cd951f4dab9
actnia <45020514+actnia@users.noreply.github.com>
2018-12-06 18:11:45 +0900

    Update temp_client.py

--- temp_client.py
+++ temp_client.py
@@ -1,5 +1,6 @@
 #임시 클라이언트
 import socket
+import threading
 
 # 접속하고자 하는 서버의 주소 및 포트
 server_ip = '127.0.0.1'
@@ -10,8 +11,33 @@ address = (server_ip, server_port)
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysock.connect(address)  # 서버에 접속
 
-data = mysock.recv(1024)
-print(data.decode('utf-8'))
+#입력받아 서버에 전송하는 함수 / 채팅함수
+#에러 코드
+#exit를 입력하면 정상적으로 스레딩이 종료되야 함. 현재는 스크롤이 무한히 내려가는 중
+def chat(mysocket):
+    while True:
+        try:
+            w=input()
+            mysocket.send(w.encode('utf-8'))
+            if w=='exit':
+                break
+        except OSError:
+            print("Error!")
+            mysocket.close()
+            break
+
+def ready():
+    Ch=threading.Thread(target=chat, args=(mysock,))
+    Ch.start()
+    while True:
+        try:
+            data=mysock.recv(1024)
+        except OSError:
+            break
+        print(data.decode('utf-8'))
+        if data.decode('utf-8')=="start":
+            break
+
+ready()
 now=input()
-print(type(now))
 mysock.send(now.encode('utf-8'))
