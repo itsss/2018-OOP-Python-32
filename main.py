30e85142e9c0296f7ece4afaf9f16d17f778cd1d
Taewon Kang <itschool@itsc.kr>
2018-12-15 15:32:01 +0900

    (18.12.15) 오류 수정 3

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os, random
 
 item_list = {'커피': 5, '밀가루': 5, '희토류': 5, '석유': 5, '소고기': 5, '시멘트': 5, '알루미늄': 5, '강철': 5}
 turns = 5
-             
+
 def trading(item_list):
     pass
 '''
@@ -16,44 +16,44 @@ def get_price_change(item_list):
     1. 물건 시세 변동여부와 공급/수요 여부 텍스트 파일
     2. 가격 변동 여부(-1/1) 파일 만들기
     '''
-global turns
+    # global turns
 
-flist = os.listdir('price_data')
-
-for file in flist:
-    fr = open('price_data\\' + file, 'r')
-    lines = f.readlines()
-    random.shuffle(lines)
-    fr.close()
-
-    fw = open('price_data\\' + file, 'w')
-    fw.write(lines)
-    fw.close()
-
-for cnt in range(turns):
-    new_text = 'price_text_' + str(cnt) + '.txt'
-    new_updown = 'price_updown_' + str(cnt) + '.txt'
-
-    fw_text = open('price_text\\' + new_text, 'w')
-    fw_updown = open('price_updown\\' + new_updown, 'w')
+    flist = os.listdir('price_data')
 
     for file in flist:
         fr = open('price_data\\' + file, 'r')
         lines = fr.readlines()
-        price = lines[cnt]
-        fw_text.write(price)
-
-        tag = price.split('(')[1]
-        change = tag.split()[1]
-        if tag.startswith('수요 증가') or tag.startswith('공급 감소'):
-            fw_updown.write('가격 증가')
-        else:
-            fw_updown.write('가격 감소')
+        random.shuffle(lines)
         fr.close()
 
-    fw_text.close()
-    fw_updown.close()
-             
+        fw = open('price_data\\' + file, 'w')
+        fw.write(lines)
+        fw.close()
+
+    for cnt in range(turns):
+        new_text = 'price_text_' + str(cnt) + '.txt'
+        new_updown = 'price_updown_' + str(cnt) + '.txt'
+
+        fw_text = open('price_text\\' + new_text, 'w')
+        fw_updown = open('price_updown\\' + new_updown, 'w')
+
+        for file in flist:
+            fr = open('price_data\\' + file, 'r')
+            lines = fr.readlines()
+            price = lines[cnt]
+            fw_text.write(price)
+
+            tag = price.split('(')[1]
+            change = tag.split()[1]
+            if tag.startswith('수요 증가') or tag.startswith('공급 감소'):
+                fw_updown.write('가격 증가')
+            else:
+                fw_updown.write('가격 감소')
+            fr.close()
+
+        fw_text.close()
+        fw_updown.close()
+
 def print_price_text(turns):
     '''
     물건 시세 변동 여부와 공급/수요 여부 텍스트 파일 출력하기
@@ -63,7 +63,7 @@ def print_price_text(turns):
     fr = open('price_text\\' + file, 'r')
     while True:
         line = fr.readline()
-        if not line: 
+        if not line:
             break
         print(line)
     fr.close()
@@ -81,7 +81,7 @@ def read_price_text(val):
         # coffee-1 | 브라질이 최악의 가뭄을 경험하고 있다.| 공급 | image/coffee/drought.png
 
     return list
-       
+
 def print_ind_score():
     pass
 '''
@@ -113,7 +113,7 @@ for i in range(turns):
     trading(item_list)
     print_ind_score()
     print_all_score()
-    
+
 final_score()
 
 '''
